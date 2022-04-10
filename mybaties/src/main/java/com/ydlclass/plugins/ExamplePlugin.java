48c11555b4893c3d74cf9ba9180817bdd3a53e72
hqh <210401532@qq.com>
2022-04-10 23:50:22 +0800

    哈哈

--- mybaties/src/main/java/com/ydlclass/plugins/ExamplePlugin.java
+++ mybaties/src/main/java/com/ydlclass/plugins/ExamplePlugin.java
@@ -1,7 +1,9 @@
 package com.ydlclass.plugins;
 
 import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.SqlSource;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
@@ -9,6 +11,7 @@ import org.apache.ibatis.plugin.Signature;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
 
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -22,13 +25,30 @@ public class ExamplePlugin implements Interceptor {
     private Properties properties = new Properties();
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
-        // implement pre processing if need
-        System.out.println("执行之前");
-        Object returnObject = invocation.proceed();
-        // implement post processing if need
-        System.out.println("执行之后");
-        Object someProperty = properties.get("someProperty");
-        System.out.println(someProperty);
+        //拿到原始的sql
+        Object[] args = invocation.getArgs();
+        MappedStatement ms = (MappedStatement) args[0];
+        BoundSql boundSql = ms.getBoundSql(args[1]);
+        String sql = boundSql.getSql();
+//        改造sql
+        String newsql=sql+" limit 1,10";
+        SqlSource sqlsource= parameterObject -> new BoundSql(ms.getConfiguration(),newsql,null,args[1]);
+
+
+        MappedStatement mappedstatement = new MappedStatement.Builder(ms.getConfiguration(), ms.getId() + "byPage", sqlsource, ms.getSqlCommandType()).cache(ms.getCache()).keyGenerator(ms.getKeyGenerator()).resultMaps(ms.getResultMaps()).build();
+
+//        调用方法
+        Executor executor = (Executor)invocation.getTarget();
+        List<Object> returnObject = executor.query(mappedstatement, args[1], (RowBounds) args[2], (ResultHandler) args[3]);
+
+
+//        System.out.println("执行之前");
+//        System.out.println("1");
+//        Object returnObject = invocation.proceed();
+//        // implement post processing if need
+//        System.out.println("执行之后");
+//        Object someProperty = properties.get("someProperty");
+//        System.out.println(someProperty);
         return returnObject;
     }
     @Override
